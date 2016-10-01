dac9406e3ed80f281551fcf205cffde991d9ef5e
Rahul Gupta <neortls007idev@users.noreply.github.com>
2016-10-01 16:55:14 +0530

    sample program
    if you mean something like this, then i think we will need to define individual functions for each character and also use a pre-initialised array that maps the engligh language phonetics to the hindi language phonetics and in case of ambiguity allow the user to choose.It's really just troublesome.

---
+++ Rishabh.java
@@ -0,0 +1,28 @@
+import java.applet.*;
+import java.awt.*;
+
+public class Rishabh extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawLine(10,20,140,20);
+g.drawLine(25,20,25,60);
+g.drawLine(10,25,25,40);
+g.drawLine(10,55,25,40);
+g.drawLine(25,40,40,35);
+g.drawLine(45,40,40,32);
+g.drawArc(40,30,5,5,0,360);
+g.drawArc(30,40,30,20,90,290);
+g.drawLine(70,20,70,45);
+g.drawArc(70,40,30,8,180,180);
+g.drawLine(100,20,100,60);
+g.drawLine(70,20,100,45);
+g.drawArc(110,20,10,10,0,280);
+g.drawLine(120,25,120,49);
+g.drawArc(113,44,8,8,70,270);
+g.drawLine(120,44,130,44);
+g.drawLine(130,20,130,60);
+}
+}
+//<applet code="Rishabh" width = 200 height = 300></applet>
+ 
